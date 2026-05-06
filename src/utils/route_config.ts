7f2f0ebd66672ec030639aa33ca8650a4e4f234d
Chimaobi Onuoha <davidkings1604@gmail.com>
2026-05-06 09:43:39 -0400

    feat: sitemap

---
+++ src/utils/route_config.ts
@@ -0,0 +1,13 @@
+export const routesConfig = {
+  static: [
+    "/",
+    "/contact_us",
+    "/explore_trips",
+    "/about_us",
+  ],
+
+  dynamic: {
+    wishlist: "/wishlist",
+    destination: "/destination_details",
+  },
+};
